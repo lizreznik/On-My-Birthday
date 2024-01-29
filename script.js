f31e0d3192b45ba0e1fe4e07e67ef5e0394a20aa
lizreznik <525ereznik@frhsd.com>
2024-01-29 10:40:07 -0500

    kk

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function year2007(){
-document.getElementById('response-date').innerText= 'September 14 2007'
-document.getElementById('response-text').innerText= 'i was born'
+function year2007() {
+    document.getElementById('response-date').innerText = 'September 14 2007'
+    document.getElementById('response-text').innerText = 'i was born'
 }
 
-function year2008(){ 
-document.getElementById('response-date').innerText= 'September 14 2008'
-document.getElementById('response-text').innerText= 'on this date'
+function year2008() {
+    document.getElementById('response-date').innerText = 'September 14 2008'
+    document.getElementById('response-text').innerText = 'on this date'
+}
+
+function year2009() {
+    document.getElementById('response-date').innerText = 'September 14 2009'
+    document.getElementById('response-text').innerText = 'hello world'
+}
+
+function year2010() {
+    document.getElementById('response-date').innerText = 'September 14 2010'
+    document.getElementById('response-text').innerText = 'on this date'
+}
+
+function year2011() {
+    document.getElementById('response-date').innerText = 'September 14 2011'
+    document.getElementById('response-text').innerText = 'yurrr'
 }
