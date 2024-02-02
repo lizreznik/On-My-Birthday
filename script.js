a54d7b1fa7534b1f9b398d15bd43ee82caa3750e
lizreznik <525ereznik@frhsd.com>
2024-02-02 11:12:16 -0500

    removed hero

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function year2007() {
-    document.getElementById('response-date').innerText = 'September 14 2007'
+    document.getElementById('response-date').innerText = '2007'
     document.getElementById('response-text').innerText = 'i was born'
+    document.getElementById("bg-hero-img").src = 'IMG_0255 (1).jpg'
 }
 
 function year2008() {
-    document.getElementById('response-date').innerText = 'September 14 2008'
+    document.getElementById('response-date').innerText = '2008'
     document.getElementById('response-text').innerText = 'on this date'
 }
 
 function year2009() {
-    document.getElementById('response-date').innerText = 'September 14 2009'
+    document.getElementById('response-date').innerText = '2009'
     document.getElementById('response-text').innerText = 'hello world'
 }
 
