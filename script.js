65fb9d22dc87230215168f0b95817ae98293c775
lizreznik <525ereznik@frhsd.com>
2024-02-06 09:11:50 -0500

    ALL INFO ADDED

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
+function year1814() {
+    document.getElementById('response-date').innerText = '1814'
+    document.getElementById('response-text').innerText = 'Francis Scott Key, born on August 1, 1779, in Maryland, wrote a poem called "The Defence of Fort McHenry" in 1814, inspired by the sight of a lone U.S. flag still flying over Fort McHenry at daybreak. The poem, originally titled "The Defence of Fort M Henry," was set to music and became Americas national anthem in 1931. Key was inspired by the sight of the British bombing the fort, which was later set to music by John Stafford Smith. The poem was later referred to as "The Star-Spangled Banner" and was adopted as the national anthem by President Woodrow Wilson in 1916. Key died of pleurisy on January 11, 1843. The poem was later set to music and became Americas national anthem in 1931.'
+    document.getElementById("pictures").src = 'american-flag.jpg'
+    
+}
+
 function year2007() {
     document.getElementById('response-date').innerText = '2007'
     document.getElementById('response-text').innerText = 'i was born'
-    document.getElementById("bg-hero-img").src = 'IMG_0255 (1).jpg'
+    document.getElementById("pictures").src = 'IMG_0255 (1).jpg'
 }
 
-function year2008() {
-    document.getElementById('response-date').innerText = '2008'
-    document.getElementById('response-text').innerText = 'on this date'
+function year1939() {
+    document.getElementById('response-date').innerText = '1939'
+    document.getElementById('response-text').innerText = 'The VS-300, the worlds first practical helicopter, took flight on September 14, 1939, in Stratford, Connecticut. Designed by Igor Sikorsky, it was the first to incorporate a single main rotor and tail rotor design. The first free flight took place on May 13, 1940, and the 28-foot diameter, three-blade rotor allowed for variable pitch of blades with a blade speed of 250 to 300 mph. The VS-300 laid the foundation for the first production helicopters and became the standard for helicopter manufacturing worldwide. Sikorsky submitted a patent application for a direct lift aircraft in 1931, which was granted on March 19, 1935. The VS-300 is on display at the Henry Ford Museum.'
+    document.getElementById("pictures").src = 'VS-300-e1371603228756.jpg'
+}
+
+function year2017() {
+    document.getElementById('response-date').innerText = '2017'
+    document.getElementById('response-text').innerText = 'Hurricane Florence, a slow-moving category one hurricane, made landfall on September 14, 2018, and over the next two days, it produced record-breaking rainfall across eastern North Carolina and northeastern South Carolina. Some locations recorded over 30 inches of rain, exceeding the highest single-storm rainfall amounts in the state. A station in Loris, SC recorded 23.63 inches of rain, setting a new state tropical cyclone rainfall record for South Carolina.'
+    document.getElementById("pictures").src = '_103445927_florence1400_15_09-nc.png'
 }
 
 function year2009() {
     document.getElementById('response-date').innerText = '2009'
-    document.getElementById('response-text').innerText = 'hello world'
-}
-
-function year2010() {
-    document.getElementById('response-date').innerText = '2010'
-    document.getElementById('response-text').innerText = 'on this date'
-}
-
-function year2011() {
-    document.getElementById('response-date').innerText = '2011'
-    document.getElementById('response-text').innerText = 'yurrr'
+    document.getElementById('response-text').innerText = 'Juan Martin del Potro, a 20-year-old from Argentina, defeated Roger Federer in his first Grand Slam final, a thrilling four-hour, six-minute final. The loss ended Federers 40-match US Open winning streak and denied him the honor of winning the same Grand Slam event six consecutive times. The win also made del Potro the tallest man ever to win a Grand Slam tournament, as he came back from two-sets-to-one down. The event marked the first five-set final since 1999 and the first to run longer than four hours since 1988.'
+    document.getElementById("pictures").src = 'f_delpotro2009.jpg'
 }
